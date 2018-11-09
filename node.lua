3b021cc4a48250d67b187770712d875cd75ddb4d
Christian Pointner <equinox@spreadspace.org>
2018-11-09 23:47:24 +0100

    try scaling

--- node.lua
+++ node.lua
@@ -36,7 +36,7 @@ NATIVE_HEIGHT = NATIVE_HEIGHT or 1080
 
 node.set_flag("no_clear")
 
-gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+gl.setup(NATIVE_WIDTH/2, NATIVE_HEIGHT/2)
 
 local font = CONFIG.font
 local logo = CONFIG.logo:ensure_loaded()
