e234051318d63e4083f84129f16c9646c2f88aad
Christian Pointner <equinox@spreadspace.org>
2018-11-10 16:01:09 +0100

    major rewrite of node.lua the code is much nicer and cleaner now drop licence info for node.lua because this is now completly different from its origin

--- node.lua
+++ node.lua
@@ -1,38 +1,8 @@
---[[
-
-  this based on node.lua from:
-  https://github.com/info-beamer/package-installation-help
-  this repo has the following license:
-
-
-  Copyright (C) 2016 Florian Wesch <fw@dividuum.de>
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-
-]]--
-
 node.alias "install"
 util.init_hosted()
-util.noglobals()
 
-NATIVE_WIDTH = NATIVE_WIDTH or 1920
-NATIVE_HEIGHT = NATIVE_HEIGHT or 1080
+-- this is only supported on the Raspi....
+util.noglobals()
 
 node.set_flag("no_clear")
 
@@ -42,88 +12,69 @@ local font = CONFIG.font
 local logo = CONFIG.logo:ensure_loaded()
 local title = CONFIG.title
 local line = resource.create_colored_texture(1,1,1,0.5)
-local serial = sys.get_env("SERIAL")
+local serial = sys.get_env("SERIAL") or "<no serial>"
+
+local colors = {
+   black  = { 0.0, 0.0, 0.0, 1.0},
+   white  = { 1.0, 1.0, 1.0, 1.0},
+   grey   = { 0.5, 0.5, 0.5, 1.0},
+   red    = { 1.0, 0.0, 0.0, 1.0},
+   green  = { 0.0, 1.0, 0.0, 1.0},
+   orange = { 1.0, 0.7, 0.0, 1.0}
+}
+
+local keys = {
+   _spacer_ = "XXXXXXXXXXXXXXXX",
+   network  = "Network config",
+   ethmac   = "Ethernet MAC",
+   ethip    = "Ethernet IPv4",
+   wlanmac  = "WiFi MAC",
+   wlanip   = "WiFi IPv4",
+   gw       = "Gateway",
+   online   = "Online status"
+}
 
 local values = {}
 util.data_mapper {
-    ["update/(.*)"] = function(key, val)
-        values[key] = val
-    end
+   ["update/(.*)"] = function(k, v)
+      values[k] = v
+   end
 }
 
 local function draw_info()
-    local size = math.floor(HEIGHT/16)
-    local y = size
+   local s = math.floor(HEIGHT/16)
+   local y = s
 
-    local x_spacing, y_spacing = size * 0.3, size * 0.3
-    local l_x, l_y = logo:size()
-    l_x = l_x * (size*4/l_y)
-    logo:draw(size, y, size+l_x, y+size*4)
+   local logo_x, logo_y = logo:size()
+   logo_x = logo_x * (s*4/logo_y)
+   logo:draw(s, y, s+logo_x, y+s*4)
 
-    local width_title = font:width(title, size)
-    font:write(WIDTH*0.5-width_title/2, y, title, size, 1, 1, 1, 1)
-    y = y + size + y_spacing
+   local title_w = font:width(title, s)
+   font:write(s*0.7 + WIDTH*0.5-title_w/2, y, title, s, unpack(colors.white))
+   y = y + s + s * 0.3
 
-    local size_serial = size*2.5
-    local width_serial = font:width(serial, size_serial)
-    font:write(WIDTH*0.5-width_serial/2, y, serial, size_serial, 1, 0.77, 0, 1)
-    y = y + size_serial + 2*y_spacing
+   local serial_s = s*2.5
+   local serial_w = font:width(serial, serial_s)
+   font:write(s*0.7 + WIDTH*0.5-serial_w/2, y, serial, serial_s, unpack(colors.orange))
+   y = y + serial_s + s * 0.7
 
-    line:draw(0, y-1, WIDTH, y+1)
-    y = y + 3*y_spacing
+   line:draw(0, y-1, WIDTH, y+1)
+   y = y + s
 
-    local k_x, v_x = WIDTH/2-font:width("XXXXXXXXXXXXXXXX", size), WIDTH/2
-    local function key(str)
-        font:write(k_x, y, str, size, 1,1,1,.5)
-    end
-    local function val(str, col)
-        col = col or {1,1,1,.5}
-        font:write(v_x, y, str, size, unpack(col))
-        y = y + size*1.1
-    end
+   local k_x, v_x = WIDTH/2-font:width(keys._spacer_, s), WIDTH/2
+   local function print_kv(k, v, c)
+      font:write(k_x, y, k, s, unpack(colors.grey))
+      font:write(v_x, y, v or "-", s, unpack(c or colors.grey))
+      y = y + s*1.1
+   end
 
-    if values.network then
-        key "Network config"
-        val(values.network)
-    end
-
-    if values.ethmac then
-        key "Ethernet MAC"
-        val(values.ethmac)
-    end
-
-    if values.ethip then
-        key "Ethernet IPv4"
-        val(values.ethip)
-    end
-
-    if values.wlanmac then
-        key "WiFi MAC"
-        val(values.wlanmac)
-    end
-
-    if values.wlanip then
-        key "WiFi IPv4"
-        val(values.wlanip)
-    end
-
-    if values.gw then
-        key "Gateway"
-        val(values.gw)
-    end
-
-    if values.online then
-        key "Online status"
-        local col = {1,0,0,1}
-        if values.online == "online" then
-            col = {0,1,0,1}
-        end
-        val(values.online, col)
-    end
-
+   for _,k in ipairs({"network", "ethmac", "ethip", "wlanmac", "wlanip", "gw"}) do
+      print_kv(keys[k], values[k])
+   end
+   print_kv(keys.online, values.online, (values.online == "online") and colors.green or colors.red)
 end
 
 function node.render()
-    gl.clear(0,0,0,1)
-    draw_info()
+   gl.clear(unpack(colors.black))
+   draw_info()
 end
